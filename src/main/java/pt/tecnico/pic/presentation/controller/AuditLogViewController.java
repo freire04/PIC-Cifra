e2a9d8b67723eb679b64e11184d2b758cb6266eb
Miguel Mateus <miguel.lousteau@gmail.com>
2026-06-23 15:47:05 +0100

    cenas temporarias inicias

--- src/main/java/pt/tecnico/pic/presentation/controller/AuditLogViewController.java
+++ src/main/java/pt/tecnico/pic/presentation/controller/AuditLogViewController.java
@@ -1,5 +1,6 @@
 package pt.tecnico.pic.presentation.controller;
 
+import java.util.Collections;
 import java.util.List;
 
 import pt.tecnico.pic.application.AppController;
@@ -11,27 +12,54 @@ public class AuditLogViewController {
 
     private final AppController appController;
     private final SceneManager sceneManager;
+
     private LogFilter currentFilter;
 
-    public AuditLogViewController(AppController appController, SceneManager sceneManager) {
+    public AuditLogViewController(AppController appController,
+                                  SceneManager sceneManager) {
         this.appController = appController;
         this.sceneManager = sceneManager;
         this.currentFilter = new LogFilter();
     }
 
-    public void initialize() {}
+    public void initialize() {
+        loadLogs();
+    }
 
-    public void loadLogs() {}
+    public void loadLogs() {
+        try {
+            List<LogDTO> logs = appController.getLogs(currentFilter);
+            showLogs(logs);
 
-    public void onApplyFilter() {}
+        } catch (Exception e) {
+            showError(e.getMessage());
+        }
+    }
 
-    public void onClearFilter() {}
+    public void onApplyFilter() {
+        loadLogs();
+    }
 
-    public void onRefreshClicked() {}
+    public void onClearFilter() {
+        currentFilter = new LogFilter();
+        loadLogs();
+    }
 
-    public void showLogs(List<LogDTO> logs) {}
+    public void onRefreshClicked() {
+        loadLogs();
+    }
 
-    public void showError(String message) {}
+    public void showLogs(List<LogDTO> logs) {
+        if (logs == null) {
+            logs = Collections.emptyList();
+        }
+
+        System.out.println("Loaded " + logs.size() + " logs.");
+    }
+
+    public void showError(String message) {
+        System.err.println("Error loading logs: " + message);
+    }
 
     public LogFilter getCurrentFilter() {
         return currentFilter;
@@ -40,5 +68,5 @@ public class AuditLogViewController {
     public void setCurrentFilter(LogFilter currentFilter) {
         this.currentFilter = currentFilter;
     }
-
+    
 }
